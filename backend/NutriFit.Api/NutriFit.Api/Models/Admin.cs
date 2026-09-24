1c1e68e2f6f49025bedcde61499a7f02fa7e0a6d
ShivamParve24 <131367358+Yshivamparve0204@users.noreply.github.com>
2026-09-24 14:28:31 +0530

    Update Admin.cs

--- backend/NutriFit.Api/NutriFit.Api/Models/Admin.cs
+++ backend/NutriFit.Api/NutriFit.Api/Models/Admin.cs
@@ -4,6 +4,6 @@
     public string? Name { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.Now
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
 
